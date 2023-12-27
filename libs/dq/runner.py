8772051d696b17c0054f687c788dc70a489d51c7
utkarsh-legoai <utkarsh.rai@legoai.com>
2023-12-27 16:12:38 +0530

    Remove unnecessary folders

--- libs/dq/runner.py
+++
@@ -1,9 +0,0 @@
-from src.rules.RulesABC import RulesABC
-
-def run(df, config = []):
-    ruleSummary = {}
-    for rule in config["rules"]:
-        inst = RulesABC(df, rule["ruleName"].lower(), rule["params"] )
-        res = inst.getSummary()
-        ruleSummary[rule["ruleAliasName"]] = res  
-    return ruleSummary
--- testapp/test.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-sys.path.append("../")
-print(dir(sys))
-print(dir())
-from app.models import all_models
